27f103774a75775e2fbdba94774e8a68aa507898
Ashen <akavindu288@gmail.com>
2025-10-28 08:50:03 +0530

    Update EmployeeSeeder.php

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Employee; // Employee model ಅನ್ನು ಇಂಪೋರ್ಟ್ ಮಾಡಿ
+use App\Models\Employee; 
 use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
 
